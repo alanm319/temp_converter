51ef09aed3f6f92234de74491162e04f5a6d71dd
alanm319 <alanm319@gmail.com>
2024-05-20 15:25:54 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::io;
+fn main() {
+
+//  println!("Enter 0 for Fehrenheit to Celsius, or 1 for Celsius to Fehrenheit");
+//  let mut flag = String::new();
+//  io::stdin()
+//      .read_line(&mut flag)
+ //     .expect("Failed to read line");
+  
+ // let flag: i32 = flag.trim().parse().expect("Please type a numer!");
+ 
+  
+  println!("Enter a temperature in Fehrenheit:"); 
+  let mut temp = String::new();
+  io::stdin()
+      .read_line(&mut temp)
+      .expect("Failed to read line");
+  
+  let temp: f64 = temp.trim().parse().expect("Please type a numer!");
+  let converted_temp = temp_conversion(temp);
+
+  println!("{temp} in celsius is {converted_temp}");
+
+
+
+}
+
+fn temp_conversion(temp: f64) -> f64{
+    
+    (temp-32.0) * (5.0/9.0)
+}
